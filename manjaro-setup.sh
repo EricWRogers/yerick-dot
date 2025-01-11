fe1503029692cfe4f284e06472fa416ff4e4eb33
EricWRogers <ericwilliamrogers@gmail.com>
2025-01-11 00:26:46 -0600

    manjaro

---
+++ manjaro-setup.sh
@@ -0,0 +1,33 @@
+
+git config --global user.name "EricWRoges"
+git config --global user.email "ericwilliamrogers@gmail.com"
+
+# update & upgrade
+sudo pacman -Syu
+
+# music
+sudo pacman -S elisa
+
+# canis
+sudo pacman -S gcc gdb clang ccache cmake make
+
+# vscode
+sudo pamac install --no-confirm base-devel visual-studio-code-bin
+
+# unityhub
+sudo pamac install --no-confirm unityhub
+
+# google chrome
+sudo pamac install --no-confirm google-chrome
+
+# renderdoc
+sudo pamac install --no-confirm renderdoc
+
+# kasts
+sudo pamac install --no-confirm kasts
+
+# lazygit
+sudo pamac install --no-confirm lazygit
+
+# blender lts
+sudo pamac install --no-confirm blender-4.2-bin
